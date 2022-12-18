48e3883ab3cd5c865e2449d7ab4588d6699f1dbb
Dima <dim@kuch.in>
2022-12-18 21:35:23 +0300

    Simplify regexp and add reading from stdin

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Simple python script to solve equations and systems of equations
 
 **Usage:** solver.py data
 
-_Where `data` is a file containig equations and systems of equations separated by new line_
+_Where `data` is a file containig equations and systems of equations separated by new line. Any lowercase character a..z is treated as variable._
     
--- solve.py
+++ solve.py
@@ -4,36 +4,36 @@ import sys
 if len(sys.argv) > 1:
     fname = sys.argv[1]
 else:
-    print(f"Usage: {sys.argv[0]} file", file=sys.stderr)
-    sys.exit(1)
+    fname = ''
 
 from sympy import *
 import string
 import re
 
-# set variables names a = 'a', b = 'b', etc.
+# Set the names of the variables: a = 'a', b = 'b', etc.
 for ch in string.ascii_lowercase:
     globals()[ch] = symbols(ch)
 
+if fname:
+    f = open(fname, 'r')
+else:
+    f = sys.stdin
+
 system = []
-with open(fname, 'r') as f:
-    while s:= f.readline():
-        if s != '\n':
-            s  = s.split('\n')[0]
-            print(s)
-            s = re.sub('([a-z]{1})', r'\1*', s)
-            s = re.sub('\*$', '', s)
-            s = re.sub('([a-z])(\*)([+-=/\s])', r'\1\3', s)
-            s = re.sub('(\d+)([^0-9+-/*=\)\s])', r'\1*\2', s)
-            # print('parsed line:', s)
-            eq = s.split('=')
-            system.append(Eq(eval(eq[0]), eval(eq[1])))
-        else:
-            if system:
-                print(solve(system))
-                print()
-            system = []
+while s:= f.readline():
+    if s != '\n':
+        s  = s.split('\n')[0]
+        if fname: print(s)
+        # Add '*' between variables and numbers: 10xy --> 10*x*y
+        s = re.sub('([a-z0-9])(?=[a-z(])', r'\1*', s)
+        eq = s.split('=')
+        system.append(Eq(eval(eq[0]), eval(eq[1])))
     else:
         if system:
             print(solve(system))
             print()
+        system = []
+else:
+    if system:
+        print(solve(system))
+        print()
