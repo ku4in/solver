0a74e6e80dde8b83478b3a5e346c9f1ca6723fb3
Dima <dim@kuch.in>
2022-12-21 22:54:36 +0300

    Add math functions

--- solve.py
+++ solve.py
@@ -5,6 +5,14 @@ from sympy import *
 import string
 import re
 
+MATH =  [
+        'abs' , 'sqrt', 'cbrt', 'root',
+        'log' , 'ln'  , 'exp' ,
+        'pi'  , 'sin' , 'cos' , 'tan' , 'cot' , 'sec' , 'csc', 'sinc',
+        'asin', 'acos', 'atan', 'acot', 'asec', 'acsc', 'atan2',
+        'sinh', 'cosh', 'tanh', 'coth', 'sech', 'csch',
+        'asinh','acosh','atanh','acoth','asech','acsch',
+        ]
 
 if len(sys.argv) > 1:
     fname = sys.argv[1]
@@ -18,13 +26,25 @@ for ch in string.ascii_lowercase:
     globals()[ch] = symbols(ch)
 
 system = []
-while s:= fil.readline():
-    if s != '\n':
-        s  = s.split('\n')[0]
-        if fname: print(s)
+while ss:= fil.readline():
+    if ss != '\n':
+        ss  = ss.split('\n')[0]
+        if fname: print(ss)
+        # Excluding math function of subsequent processing by making them uppercase
+        dic = {}
+        for ii, mm in enumerate(MATH):
+            mat = None
+            mat = re.search(mm, ss)
+            if mat: 
+                dic[ii] = mat.group()
+                ss = re.sub(mm, f"<{ii}>", ss)
+
         # Add '*' between variables and numbers: 10xy --> 10*x*y
-        s = re.sub('([a-z0-9])(?=[a-z(])', r'\1*', s)
-        eq = s.split('=')
+        ss = re.sub('([a-z0-9)])(?=[a-z<(])', r'\1*', ss)
+        # Return math functions back
+        for ii, mm in dic.items():
+            ss = re.sub(f"<{ii}>", f"{mm}", ss)
+        eq = ss.split('=')
         system.append(Eq(eval(eq[0]), eval(eq[1])))
     else:
         if system:
